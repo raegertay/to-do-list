e6e1d7b1f6eeea7d6450fce9883f08387b0e3e82
raegertay <raegertay89@gmail.com>
2017-10-17 21:18:55 +0800

    Added presence and uniqueness validation for Tasklist

--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -2,4 +2,7 @@ class Tasklist < ApplicationRecord
 
   has_many :tasks, dependent: :destroy
 
+  validates :name, presence: { message: 'Tasklist Name is mandatory' },
+                   uniqueness: { message: 'Tasklist already exist' }
+
 end
