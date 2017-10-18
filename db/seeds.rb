e9fed169cf1dd5d8ab1758248def87076ef3f0cb
raegertay <raegertay89@gmail.com>
2017-10-18 14:22:14 +0800

    Added seeds using 'faker' gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+43.times do
+  Tasklist.create(name: Faker::Job.unique.title)
+end
