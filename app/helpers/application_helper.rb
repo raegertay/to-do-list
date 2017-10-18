fc508da8815189a372dd778f1c433ecbaa00748b
raegertay <raegertay89@gmail.com>
2017-10-18 11:40:44 +0800

    Added directional arrow using Font Awesome CDN

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,10 @@ module ApplicationHelper
   def sortable(column, title=nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-    link_to title, sort: column, direction: direction, search_term: search_term
+    icon_class = column == sort_column && sort_direction == 'asc' ? 'fa-caret-up' : 'fa-caret-down'
+    link_to sort: column, direction: direction, search_term: search_term do
+      (title + ' ' + content_tag(:i, '', class: ['fa', icon_class], 'aria-hidden': 'true')).html_safe
+    end
   end
 
 end
