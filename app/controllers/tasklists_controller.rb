cc2c15a21406aade5b98ad6c3fe73e8076088cf9
raegertay <raegertay89@gmail.com>
2017-10-18 11:48:31 +0800

    Added validation for sort column and direction to prevent sql injection

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -59,11 +59,11 @@ class TasklistsController < ApplicationController
   end
 
   def sort_column
-    params[:sort] || 'name'
+    Tasklist.column_names.include?(params[:sort]) ? params[:sort] : 'name'
   end
 
   def sort_direction
-    params[:direction] || 'asc'
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
   end
 
 end
