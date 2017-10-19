b13ca71ee991ec998f7580052420d47cee8e6f80
raegertay <raegertay89@gmail.com>
2017-10-19 14:29:54 +0800

    Added new user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'User created succesfully'
+      redirect_to tasklists_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
