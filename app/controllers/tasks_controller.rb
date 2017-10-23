d9f6d4d6440bbbae332c5dd432a4abcf8f32115a
raegertay <raegertay89@gmail.com>
2017-10-23 16:23:44 +0800

    Installed shoulda-matchers gem

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
 
   before_action :prepare_tasklist, only: [:create, :new]
+  before_action :prepare_task, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
 
-  def new
-  end
+  def new; end
 
   def create
     @task = @tasklist.tasks.new(task_params)
@@ -15,20 +15,19 @@ class TasksController < ApplicationController
     end
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to tasklists_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    @tasklist = @task.tasklist
     @task.destroy
-    redirect_to tasklists_path
+    respond_to do |format|
+      format.js
+    end
   end
 
   private
@@ -39,6 +38,10 @@ class TasksController < ApplicationController
 
   def prepare_tasklist
     @tasklist = Tasklist.find(params[:tasklist_id])
+  end
+
+  def prepare_task
+    @task = Task.find(params[:id])
   end
 
 end
